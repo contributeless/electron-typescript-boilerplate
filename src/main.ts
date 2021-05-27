3b0852eeaef1d0c7e949521d6de21720fa1ce036
Eugene Khudik <eugene.khudik@gmail.com>
2021-05-27 15:23:14 +0300

    init commit

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+ 
+
+async function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 920,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  })
+
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
+
+  mainWindow.webContents.openDevTools();
+}
+
+
+app.whenReady().then(async () => {
+  await createWindow();
+  
+  app.on('activate', async function () {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        await createWindow()
+    }
+  });
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+      app.quit()
+    }
+})
